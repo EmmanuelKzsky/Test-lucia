a703ce35f8a2d578077a5307a99e1ab5c46e8ad1
zirius666 <zion.system@hotmail.com>
2023-02-22 18:25:08 -0600

    3 files modified

--- script.js
+++ script.js
@@ -8,3 +8,26 @@ menuToggle.addEventListener('click', function () {
     menu.classList.add('menu-open');
   }
 });
+// Obtiene el botón para abrir el modal y el modal
+const openModalBtn = document.getElementById('open-modal-btn');
+const modal = document.getElementById('my-modal');
+
+// Obtiene el botón para cerrar el modal
+const closeModalBtn = modal.querySelector('.close');
+
+// Agrega un evento click al botón de abrir el modal
+openModalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+// Agrega un evento click al botón de cerrar el modal
+closeModalBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Cierra el modal cuando el usuario hace clic fuera de él
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
